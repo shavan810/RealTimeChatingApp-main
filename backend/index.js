f84aa2d0187c6c25f4dd1ad31bef22cdfaf3b763
Shavan D. Singh <creative.shavan@gmail.com>
2025-08-14 00:06:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 8000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtimechatingapp-ggoc.onrender.com",
     credentials:true
 }))
 app.use(express.json())
